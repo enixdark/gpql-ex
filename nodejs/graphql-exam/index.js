1f399d0399bb45952f4e3ac174a4ad980c479d49
enixdark <cqshinn92@gmail.com>
2016-09-03 18:46:23 +0700

    replace readline by express to show data in web

--- nodejs/graphql-exam/index.js
+++ nodejs/graphql-exam/index.js
@@ -3,18 +3,30 @@ import { graphql } from 'graphql';
 import readline from 'readline';
 import mySchema from './schema/main';
 import { db } from './db/db';
-const rli = readline.createInterface({ 
-  input: process.stdin, 
-  output: process.stdout 
-}); 
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+// const rli = readline.createInterface({ 
+//   input: process.stdin, 
+//   output: process.stdout 
+// }); 
 
-console.log(db);
-rli.question('Client request: ', inputQuery => {
-  graphql(mySchema, inputQuery, {}, { db }).then( result => {
-    console.log('Server Answer :', result.data); 
-    db().close(() => rli.close()); 
-  });
-  //rli.close();
+const app = express();
+
+app.use('/graph', graphqlHTTP({
+  schema: mySchema,
+  context: { db },
+  graphiql: true 
+}));
+
+app.listen(3000, () => {
+   console.log('Running Express.js on port 3000');
 });
+console.log(db);
+// rli.question('Client request: ', inputQuery => {
+//   graphql(mySchema, inputQuery, {}, { db }).then( result => {
+//     console.log('Server Answer :', result.data); 
+//     db().close(() => rli.close()); 
+//   });
+// });
 
 
