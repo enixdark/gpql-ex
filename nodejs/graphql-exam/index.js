fe8600405e8a4f8fe2bf716139286a066422aafd
enixdark <cqshinn92@gmail.com>
2016-09-03 16:44:17 +0700

    update graphql with promise and mongodb

--- nodejs/graphql-exam/index.js
+++ nodejs/graphql-exam/index.js
@@ -2,17 +2,19 @@ import { graphql } from 'graphql';
 //const { graphql } = require('graphql');  
 import readline from 'readline';
 import mySchema from './schema/main';
-
+import { db } from './db/db';
 const rli = readline.createInterface({ 
   input: process.stdin, 
   output: process.stdout 
 }); 
 
+console.log(db);
 rli.question('Client request: ', inputQuery => {
-  graphql(mySchema, inputQuery).then( result => {
-    console.log(`Server Answer: $result.data`); 
+  graphql(mySchema, inputQuery, {}, { db }).then( result => {
+    console.log('Server Answer :', result.data); 
+    db().close(() => rli.close()); 
   });
-  rli.close();
+  //rli.close();
 });
 
 
--- nodejs/graphql-exam/schema/main.js
+++ nodejs/graphql-exam/schema/main.js
@@ -1,19 +1,42 @@
-
 import { 
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
 } from 'graphql';
-
+const roll = () => Math.floor(6 * Math.random()) + 1;
+import { db } from '../db/db';
 const queryType = new GraphQLObjectType({
   name: 'RootQuery',
   fields:{
     hello: {
       type: GraphQLString,
       resolve: () => 'hello'
-    } 
+    },
+    diceRoll: {
+      type: new GraphQLList(GraphQLInt),
+      args: {
+        count: { type: GraphQLInt }      
+      },
+      resolve: (_, args) => { 
+         let rolls = [];
+         for(let i = 0; i < args.count ; i++){
+           rolls.push(roll());         
+         }
+         return rolls;
+      }
+    },
+    usersCount: {
+      type: GraphQLInt,
+      resolve: (_, args, { db }) => {
+        return db().collection('users').count()
+        .then(usersCount => usersCount);
+      }
+    }
   }
 });
+
 const mySchema = new GraphQLSchema({
   query: queryType 
 });
